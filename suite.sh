612f023f7c366f90b4e5495df082345679384dc8
C A P T A 1 N <aryansolpankhi123@gmail.com>
2025-12-31 13:52:12 +0530

    script updated

--- suite.sh
+++ suite.sh
@@ -3,7 +3,12 @@
 domain=$1
 threads=$2
 
-timeout 7s python subdomain.py $domain $threads
+timeout 5s python subdomain.py $domain $threads
+RET=$?
+
+if [ $RET -eq 124 ]; then
+    echo -e "\e[31m[-]\e[0m Subdomains not found"
+fi
 
 python dirbuster.py http://$domain $threads .php
 
